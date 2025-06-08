d88e7472d527381b3621e0fc9d06277b82229296
Nandana Nambiar <nandananambiar18@gmail.com>
2025-06-08 13:30:58 +0530

    Updated app.py and Dockerfile for proper deployment

--- aes-app/app.py
+++ aes-app/app.py
@@ -321,7 +321,13 @@ def about():
 def goal():
     return render_template('goal.html')
 
+
+#HEALTH CHECK ROUTE FOR RENDER
+@app.route('/health')
+def health():
+    return "OK", 200
+
 # ========== Main ==========
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port)
